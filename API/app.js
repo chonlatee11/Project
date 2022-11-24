07a02dd14dac13c4f4539caf96ec663aa2450d6f
chonlatee11 <chonlatee1129@gmail.com>
2022-11-24 11:04:46 +0700

    24112022

--- API/app.js
+++ API/app.js
@@ -1,42 +1,60 @@
 var express = require('express')
 var cors = require('cors')
 var app = express()
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
 app.use(cors())
 
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
+var mysql = require("mysql");
+const { json } = require('express');
+var poolCluster = mysql.createPoolCluster();
+poolCluster.add("node0", {
+  host: "localhost",
+  port: "3306",
+  database: "mymariaDB",
+  user: "devchon",
+  password: "devchon101",
+  charset: "utf8mb4",
+});
+
+app.post('/register', jsonParser,  function (req, res, next) {
+  poolCluster.getConnection(function (err, connection) {
+	if (err) {
+	  console.log(err);
+	} else {
+	  connection.query("CREATE TABLE `mymariaDB`.`Researcher` (`researcherID` INT NOT NULL AUTO_INCREMENT , `Email` TEXT NOT NULL , `passWord` TEXT NOT NULL , `fName` TEXT NOT NULL , `lName` TEXT NOT NULL, `phonNumber` VARCHAR(10) NOT NULL, PRIMARY KEY (`researcherID`)) ENGINE = InnoDB CHARSET=utf8mb3 COLLATE utf8mb3_general_ci;;", function (err, rows) {
+		if (err) {
+		  res.json({err})
+		} else {
+		  connection.release();
+      res.json({rows})
+		}
+	  });
+	}
+  });
+})
+
+app.post('/inserttable', jsonParser,  function (req, res, next) {
+  poolCluster.getConnection(function (err, connection) {
+	if (err) {
+	  console.log(err);
+	} else {
+	  connection.query("INSERT INTO User (UserName, Password, fName, lName, PhoneNumber, Address) VALUES (?, ?, ?, ?, ?, ?);", 
+    [req.body.userName, req.body.passWord, req.body.fName, req.body.lName, req.body.phoneNumber, req.body.address] ,function (err, rows) {
+		if (err) {
+		  res.json({err})
+		} else {
+		  connection.release();
+      res.json({rows})
+		}
+	  });
+	}
+  });
 })
 
 app.listen(3030, function () {
   console.log('CORS-enabled web server listening on port 3030')
 })
-
-// var mysql = require("mysql");
-// var poolCluster = mysql.createPoolCluster();
-// poolCluster.add("node0", {
-//   host: "localhost",
-//   port: "3306",
-//   database: "mymariaDB",
-//   user: "devchon",
-//   password: "devchon101",
-//   charset: "utf8mb4",
-// });
-
-// poolCluster.getConnection(function (err, connection) {
-// 	if (err) {
-// 	  console.log(err);
-// 	} else {
-// 	  connection.query("SELECT * FROM example", function (err, rows) {
-// 		if (err) {
-// 		  console.log(err);
-// 		} else {
-// 		  console.log("ALL FREE: ");
-// 		  console.log(rows);
-// 		  connection.release();
-// 		}
-// 	  });
-// 	}
-//   });
 
   
